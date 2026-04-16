475c9f02fb9b4af9cc87e408ea4884b5fef40471
cris9495 <crisnita204@utp.edu.co>
2026-04-15 23:20:07 -0500

    adjust report

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // Set default environment to dev if not provided
       const envName = config.env.environment || 'dev';
       config.env.apiUrl = `${config.baseUrl}/${envName}`;
       return config;
     },
-    video: false,
-    screenshotOnRunFailure: false
+    // Reporter Configuration
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
+    },
+    video: true, // Videos are also part of the "testing report"
+    screenshotOnRunFailure: true
   },
 });
